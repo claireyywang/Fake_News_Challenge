63d67ba203147af46c75b47f567d7ad4af67c3d8
Graham Mosley <gmosley@seas.upenn.edu>
2018-04-23 00:05:12 -0400

    [extension2] both stages for dev

---
+++ milestone_4/extension2.py
@@ -0,0 +1,29 @@
+from extended_setup import *
+from sklearn.svm import SVC 
+
+tr_stances_file = '../dataset/train_stances.csv'
+tr_bodies_file = '../dataset/train_bodies.csv'
+dev_stances_file = '../dataset/dev_stances.csv'
+dev_bodies_file = '../dataset/dev_bodies.csv'
+test_stances_file = '../dataset/test_stances.csv'
+test_bodies_file = '../dataset/test_bodies.csv'
+
+dev_pred_file = 'output/dev_predictions.csv'
+test_pred_file = 'output/test_predictions.csv'
+
+print('loading data...')
+train_data = FNCData(tr_stances_file, tr_bodies_file)
+dev_data = FNCData(dev_stances_file, dev_bodies_file)
+test_data = FNCData(test_stances_file, test_bodies_file)
+
+bow_vectorizer, tfreq_vectorizer, tfidf_vectorizer = create_vectors(train_data, dev_data, test_data, lim_unigram=5000)
+
+# extract features and labels
+print('extracting features...')
+train_X, train_y = pipeline_train(train_data, bow_vectorizer, tfreq_vectorizer, tfidf_vectorizer)
+dev_X, dev_y = pipeline_dev(dev_data, bow_vectorizer, tfreq_vectorizer, tfidf_vectorizer)
+test_X = pipeline_test(test_data, bow_vectorizer,tfreq_vectorizer, tfidf_vectorizer)
+
+print('training  data...')
+
+print(train_y)
--- milestone_4/extension3.py
+++ milestone_4/extension3.py
@@ -50,11 +50,43 @@ train_y_stage_one = convert_to_stage_one(train_y)
 dev_y_stage_one = convert_to_stage_one(dev_y)
 
 clf_stage_one = LogisticRegression()
-clf_stage_one.fit(train_X, train_y)
+clf_stage_one.fit(train_X, train_y_stage_one)
+
+# train model for stage 2
+train_X_stage_two = []
+train_y_stage_two = []
+
+# we only train the model with examples that are related
+for xi, yi in zip(train_X, train_y):
+    if yi != stance_label['unrelated']:
+        train_X_stage_two.append(xi)
+        train_y_stage_two.append(yi)
+
+train_X_stage_two = np.array(train_X_stage_two)
+train_y_stage_two = np.array(train_y_stage_two)
+
+clf_stage_two = LogisticRegression()
+clf_stage_two.fit(train_X_stage_two, train_y_stage_two)
+
 
 print('predicting...')
+
 dev_y_pred_stage_one = clf_stage_one.predict(dev_X)
-print('stage 1 f1 score {}'.format(f1_score(dev_y_stage_one, dev_y_pred_stage_one)))
+print(dev_y_stage_one[:10])
+print(dev_y_pred_stage_one[:10])
+print('dev stage 1 f1 score {}'.format(f1_score(dev_y_stage_one, dev_y_pred_stage_one)))
 
+# we now focus only on examples we predicted to be related
+dev_X_stage_two = []
+# we also keep track of the corresponding y's
+dev_y_stage_two = []
+for i, yi in enumerate(dev_y_pred_stage_one):
+    if stage_one_map_rev[yi] == 'related':
+        dev_X_stage_two.append(dev_X[i])
+        dev_y_stage_two.append(dev_y[i])
+dev_X_stage_two = np.array(dev_X_stage_two)
+dev_y_stage_two = np.array(dev_y_stage_two)
 
+dev_y_pred_stage_two = clf_stage_two.predict(dev_X_stage_two)
 
+print('dev stage 2 f1 score {}'.format(f1_score(dev_y_stage_two, dev_y_pred_stage_two, average='micro')))
